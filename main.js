6717111c9ad33514ac689a4353fe23294260d499
MagicDog12 <creature.secret@gmail.com>
2023-03-23 01:42:07 -0300

    se arregla bug de reincio del movimiento inicial

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let contenedor = document.getElementById('pcPixelArt');
+let animacionIniciada = false;
+
 document.addEventListener('mousemove', function(evento) {
     let x = evento.clientX;
     let y = evento.clientY;
@@ -8,7 +10,15 @@ document.addEventListener('mousemove', function(evento) {
     let yRelativo = y - contenedorRect.top;
 
     let pc = document.getElementById('pcPixelArt');
+    let imgContainerSuperior = document.getElementsByClassName('imgContainerSuperior')[0];
     let xMov = (xRelativo - contenedorRect.width / 2) / 60;
     let yMov = (yRelativo - contenedorRect.height / 2) / 60;
-    pc.style.transform = 'translate(' + xMov + 'px, ' + yMov + 'px)';
+    if(!animacionIniciada) {
+        pc.style.transform = 'translate(0px, 0px)';
+        imgContainerSuperior.style.animation = 'movimientoInicial 5s ease 1';
+        imgContainerSuperior.style.animationDelay = '1.5s';
+        animacionIniciada= true;
+    } else {
+        pc.style.transform = 'translate(' + xMov + 'px, ' + yMov + 'px)';
+    }
 });
