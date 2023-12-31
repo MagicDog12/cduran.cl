4e986744565c20aa467818d1e0c9d0e386f6893e
Cristian <creature.secret@gmail.com>
2023-12-31 19:10:31 -0300

    add skills to malla page

--- cduran/src/app/pages/malla/malla.page.ts
+++ cduran/src/app/pages/malla/malla.page.ts
@@ -19,53 +19,61 @@ export class MallaPage implements OnInit {
         nombre: 'Algoritmos y Estructuras de Datos',
         descripcion: `Se aprenden algoritmos de ordenación y búsqueda.
         Además se ven estructuras de datos importantes como lo son árboles, listas,
-        hashing y grafos. Se usa python`,
+        hashing y grafos.`,
+        skills: ['Python'],
         link: ''
       },
       {
         nombre: 'Taller de Programación Competitiva',
         descripcion: `Resolución de problemas usando algoritmos y estructuras de datos.
-        Se usa C++`,
+        `,
+        skills: ['C++'],
         link: 'https://github.com/MagicDog12/Codeforces'
       },
       {
         nombre: 'Matemáticas Discretas para la Computación',
         descripcion: `Se estudia lógica, inducción, combinatoria y teoría de grafos.`,
+        skills: [],
         link: ''
       },
       {
         nombre: 'Programación de Software de Sistemas',
         descripcion: `Se aprenden conceptos básicos (tipos, punteros)
         pasando por cosas intermedias (estructuras de datos, sistema de archivos)
-        a herramientas más avanzadas como threads, sockets y paralelismo. Se usa C
+        a herramientas más avanzadas como threads, sockets y paralelismo.
         `,
+        skills: ['C'],
         link: ''
       },
       {
         nombre: 'Modelación y Computación Gráfica para Ingenieros',
         descripcion: `En estudian muchas herramientas de mecánica y lineal
-        para dibujar triángulos y formar figuras con ellos en OpenGL. Se usa Python
+        para dibujar triángulos y formar figuras con ellos en OpenGL.
         `,
+        skills: ['Python'],
         link: ''
       },
       {
         nombre: 'Metodologías de Diseño y Programación',
         descripcion: `Se enseñan patrones de diseño para OOP y
-         buenas prácticas de programación. Se usa Java
+         buenas prácticas de programación.
          `,
+         skills: ['Java'],
         link: ''
       },
       {
         nombre: 'Teoría de la Computación',
         descripcion: `Se aprenden expresiones regulares, qué son y cómo funcionan
         las maquinas de turing, y el famoso problema P vs NP.`,
+        skills: [],
         link: ''
       },
       {
         nombre: 'Bases de Datos',
-        descripcion: `Se aprende el modelo Entidad-Relación, Algebra relacional
+        descripcion: `Se aprende SQL y el modelo Entidad-Relación, Algebra relacional
         y Formas normales. Además Indexación, optimización y
-        evaluación de consultas. Inyecciones, Vistas y Privacidad. NoSQL. Se usa SQL`,
+        evaluación de consultas. Inyecciones, Vistas y Privacidad. NoSQL.`,
+        skills: ['PostgreSQL', 'MySQL'],
         link: ''
       },
       {
@@ -73,20 +81,23 @@ export class MallaPage implements OnInit {
         descripcion: `Se aprende a diseñar circuitos digitales, y probar su funcionamiento en el simulador Logisim.
         Arquitectura Risc-V, Risc y Cisc. Escribir programas en assembler Risc-V y depurarlos con ddd.
         Se estudia el diseño de cpu, memoria y sistema de entrada/salida.`,
+        skills: ['Assembler', 'C'],
         link: ''
       },
       {
         nombre: 'Lenguajes de Programación',
         descripcion: `Implementacion de un lenguaje de programación
         , uso de variables e identificadores, uso de funciones, pattern matching y testing.
-        Todo esto basado en las gramaticas y la sintaxis. Se usa Racket
+        Todo esto basado en las gramaticas y la sintaxis.
         `,
+        skills: ['Racket'],
         link: 'https://github.com/MagicDog12/Lenguajes-de-programacion'
       },
       {
         nombre: 'Ingeniería de Software',
         descripcion: `Proyecto de software dedicado a crear una aplicación
-        de reseñas de libros. Se usa Django`,
+        de reseñas de libros.`,
+        skills: ['Django', 'Python', 'Javascript', 'Bootstrap'],
         link: ''
       },
       {
@@ -94,14 +105,16 @@ export class MallaPage implements OnInit {
         descripcion: `Se aprende la sincronización de procesos concurrentes (procesos livianos),
         en un sistema operativo minimal (NanoSystem).
         También aprender diferentes estrategias de administración de los procesos
-        (scheduling). Se usa C`,
+        (scheduling).`,
+        skills: ['C'],
         link: ''
       },
       {
         nombre: 'Redes',
         descripcion: `Se aprenden protocolos de transporte y redes de datos eficientes y correctas, para
         lograr hacer un uso optimizado de Internet. Analizar el funcionamiento de los sistemas de transporte y las redes físicas utilizadas por
-        Internet (TCP/IP, por ejemplo). Se usa Python `,
+        Internet (TCP/IP, por ejemplo). `,
+        skills: ['Python'],
         link: ''
       },
       {
@@ -111,13 +124,14 @@ export class MallaPage implements OnInit {
         análisis amortizado, etc, y el uso de
         modelos avanzados de computación (memoria secundaria, algoritmos probabilísticos y
         aleatorizados.`,
+        skills: [],
         link: ''
       },
       {
         nombre: 'Ingeniería de Software II',
         descripcion: `Proyecto de software dedicado a solucionar un problema
-        de autenticación y autorización de usuarios para un cliente real.
-        Se usa Django`,
+        de autenticación y autorización de usuarios para un cliente real.`,
+        skills: ['Django', 'Python', 'Javascript', 'PostgreSQL'],
         link: ''
       },
       {
@@ -125,21 +139,22 @@ export class MallaPage implements OnInit {
         descripcion: `Se resuelven problemas de Capture The Flag mediante Esteganografía,
         Criptografía moderna, Aplicaciones Web, Análisis forense, Ingeniería reversa,
         Pwning y Osint.`,
+        skills: ['Python', 'Kali Linux'],
         link: ''
       },
       {
         nombre: 'Desarrollo de Aplicaciones Web',
         descripcion: `Desarrollo de una aplicación web de forma incremental
-        desde el front-end (HTML, CSS, JS) hasta el back-end (CGI-Python, Servlets-Java). E
-        s un curso esencial para comprender cómo funcionan los frameworks y
-        tecnologías modernas desde la base.`,
+        desde el front-end (HTML, CSS, JS) hasta el back-end (CGI-Python, Servlets-Java). Entender
+        frameworks y tecnologías modernas desde la base.`,
+        skills: ['Flask', 'Python', 'Javascript', 'PostgreSQL'],
         link: 'https://github.com/MagicDog12/Desarrollo-de-Aplicaciones-Web'
       },
       {
         nombre: 'Taller de Metodologías Ágiles de Desarrollo de Software',
-        descripcion: `Aprender y poner en práctica metodología ágil.
-        También introduce tecnologías como Docker, GitLab, Elastic Beanstalk, AWS.
+        descripcion: `Aprender y poner en práctica metodología ágil en un trabajo en equipo.
         `,
+        skills: ['Django', 'Python', 'Javascript', 'PostgreSQL', 'Docker', 'Gitlab'],
         link: ''
       },
       {
@@ -147,6 +162,7 @@ export class MallaPage implements OnInit {
         descripcion: `Implementación de algoritmos de búsqueda por similitud
         de descriptores de contenido multimedia, especialmente
         enfocado en imágenes, audio, texto y videos.`,
+        skills: ['Python', 'C++'],
         link: ''
       }
     ]
