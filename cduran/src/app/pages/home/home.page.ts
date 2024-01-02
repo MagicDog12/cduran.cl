af37f6b1bd78106003d1450bed905f59d24eea83
Cristian <creature.secret@gmail.com>
2024-01-01 22:50:43 -0300

    add new github logo

--- cduran/src/app/pages/home/home.page.ts
+++ cduran/src/app/pages/home/home.page.ts
@@ -15,6 +15,7 @@ export class HomePage implements OnInit {
   public profileImage: string = "../../assets/img/profile.jpeg";
   public linkedinLogo: string = "../../assets/img/linkedinLogo.png";
   public githubLogo: string = "../../assets/img/githubLogo.png";
+  public githubLogo2: string = "../../assets/img/github-mark.svg";
   public cvLogo: string = "../../assets/img/curriculum-Icon.png";
   public freecodecampLogo: string = "../../assets/img/freeCodeCamp-Icon.png";
 
